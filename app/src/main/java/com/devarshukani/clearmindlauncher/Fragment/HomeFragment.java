2347bbecc679ea98217811dd1cf766aa68b5c3eb
Devarsh Ukani <me.devarshukani@gmail.com>
2023-10-01 18:06:08 +0530

    Favourite Apps are visible in home screen and  can be launched

--- app/src/main/java/com/devarshukani/clearmindlauncher/Fragment/HomeFragment.java
+++ app/src/main/java/com/devarshukani/clearmindlauncher/Fragment/HomeFragment.java
@@ -1,28 +1,44 @@
 package com.devarshukani.clearmindlauncher.Fragment;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.devarshukani.clearmindlauncher.Adapter.AppListAdapter;
 import com.devarshukani.clearmindlauncher.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.lang.reflect.Method;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class HomeFragment extends Fragment implements GestureDetector.OnGestureListener{
@@ -31,6 +47,9 @@ public class HomeFragment extends Fragment implements GestureDetector.OnGestureL
     private TextView timeTextView;
     private Handler handler;
     private Runnable updateTimeRunnable;
+
+    private RecyclerView favouriteAppsRecyclerView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
@@ -63,8 +82,78 @@ public class HomeFragment extends Fragment implements GestureDetector.OnGestureL
         handler.post(updateTimeRunnable);
 
 
+
+        // favourite apps section code
+        List<AppDrawerFragment.AppListItem> selectedApps = retrieveSelectedAppsFromSharedPreferences();
+
+        favouriteAppsRecyclerView = view.findViewById(R.id.favourite_apps_recycler_view);
+        AppAdapter adapter = new AppAdapter(selectedApps);
+        favouriteAppsRecyclerView.setAdapter(adapter);
+        favouriteAppsRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+
+
         return view;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+
+        // Retrieve the updated list of selected apps from SharedPreferences
+        List<AppDrawerFragment.AppListItem> selectedApps = retrieveSelectedAppsFromSharedPreferences();
+
+        // Update the data in the adapter and notify it of the data change
+        AppAdapter adapter = (AppAdapter) favouriteAppsRecyclerView.getAdapter();
+        if (adapter != null) {
+            adapter.updateData(selectedApps);
+        }
+    }
+
+    private List<AppDrawerFragment.AppListItem> retrieveSelectedAppsFromSharedPreferences() {
+        SharedPreferences preferences = getSharedPreferences(getContext());
+        String selectedAppsJson = preferences.getString("HomeScreenFavouriteSelectedAppsList", null);
+
+        List<AppDrawerFragment.AppListItem> selectedApps = new ArrayList<>();
+
+        if (selectedAppsJson != null) {
+            try {
+                // Parse the JSON array of app names and package names
+                JSONArray jsonArray = new JSONArray(selectedAppsJson);
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    String appNameAndPackage = jsonArray.getString(i);
+                    String[] parts = appNameAndPackage.split("\\|");
+
+                    if (parts.length == 2) {
+                        AppDrawerFragment.AppListItem appItem = new AppDrawerFragment.AppListItem();
+                        appItem.name = parts[0];
+                        appItem.label = parts[1];
+
+
+                        // You can add more properties like icon if needed
+
+                        selectedApps.add(appItem);
+
+                        Log.d("DATA CHECK", appItem.name.toString());
+                        Log.d("DATA CHECK", appItem.label.toString());
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+        return selectedApps;
+    }
+
+    private static SharedPreferences getSharedPreferences(Context context) {
+        return context.getSharedPreferences("ClearMindSettings", Context.MODE_PRIVATE);
+    }
+
 
     @Override
     public void onDestroy() {
@@ -144,4 +233,69 @@ public class HomeFragment extends Fragment implements GestureDetector.OnGestureL
         }
         return false;
     }
+
+    private void launchApp(AppDrawerFragment.AppListItem app) {
+        PackageManager manager = getContext().getPackageManager();
+        Intent launchIntent = manager.getLaunchIntentForPackage(app.label.toString());
+        if (launchIntent != null) {
+            startActivity(launchIntent);
+        }
+
+
+    }
+
+    private class AppAdapter extends RecyclerView.Adapter<HomeFragment.AppAdapter.AppViewHolder> {
+
+        private List<AppDrawerFragment.AppListItem> appsList;
+
+        public AppAdapter(List<AppDrawerFragment.AppListItem> appsList) {
+            this.appsList = appsList;
+        }
+
+        @NonNull
+        @Override
+        public HomeFragment.AppAdapter.AppViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_app_list, parent, false);
+            return new HomeFragment.AppAdapter.AppViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull HomeFragment.AppAdapter.AppViewHolder holder, int position) {
+            AppDrawerFragment.AppListItem app = appsList.get(position);
+            holder.appName.setText(app.name);
+
+
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    launchApp(app);
+                }
+            });
+
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return appsList.size();
+        }
+
+        // Method to update the data in the adapter
+        public void updateData(List<AppDrawerFragment.AppListItem> newAppsList) {
+            appsList.clear();
+            appsList.addAll(newAppsList);
+            notifyDataSetChanged();
+        }
+
+        class AppViewHolder extends RecyclerView.ViewHolder {
+
+            TextView appName;
+
+            public AppViewHolder(@NonNull View itemView) {
+                super(itemView);
+                appName = itemView.findViewById(R.id.name);
+            }
+        }
+    }
+
 }
