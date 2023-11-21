247342ddba36d16ded43dba6ad550230254c9f7d
Devarsh Ukani <me.devarshukani@gmail.com>
2023-11-21 10:23:42 +0530

    Create AboutSettingsActivity.java

---
+++ app/src/main/java/com/devarshukani/clearmindlauncher/Activity/AboutSettingsActivity.java
@@ -0,0 +1,16 @@
+package com.devarshukani.clearmindlauncher.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.devarshukani.clearmindlauncher.R;
+
+public class AboutSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_settings);
+    }
+}
