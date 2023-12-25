7ab07777d7f3d49c085cc357e45317766e7f833d
Devarsh Ukani <me.devarshukani@gmail.com>
2023-12-25 21:30:36 +0530

    note added to dialog box

---
+++ app/src/main/java/com/devarshukani/clearmindlauncher/Activity/AppPauseControlsSettingsActivity.java
@@ -0,0 +1,16 @@
+package com.devarshukani.clearmindlauncher.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.devarshukani.clearmindlauncher.R;
+
+public class AppPauseControlsSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_pause_controls_settings);
+    }
+}
